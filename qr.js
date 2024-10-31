49d9307afad30faf07c603aea152941e42337e74
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-10-31 19:26:51 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Venocyber_Tech,
+	default: Gmax_Tech,
 	useMultiFileAuthState,
 	jidNormalizedUser,
 	Browsers,
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function VENOCYBER_MD_QR_CODE() {
+	async function GCYBER_BOT_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Venocyber_Tech = Venocyber_Tech({
+			let Qr_Code_By_Gmax_Tech = Venocyber_Tech({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Venocyber_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Venocyber_Techr.ev.on("connection.update", async (s) => {
+			Qr_Code_By_Gmax_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Gmax_Techr.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -58,35 +58,35 @@ router.get('/', async (req, res) => {
 				   let b64data = Buffer.from(data).toString('base64');
 				   let session = await Qr_Code_By_Venocyber_Tech.sendMessage(Qr_Code_By_Venocyber_Tech.user.id, { text: '' + b64data });
 	
-				   let VENOCYBER_MD_TEXT = `
-*_Qr Code By Venocyber Tech_*
+				   let GCYBER_BOT_TEXT = `
+*_Qr Code By Gmax Tech_*
 *_Made With 🤍_*
 ______________________________________
 *_Pair Code Connected by Venocyber Tech_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU CHOOSEN VENOCYBER-MD 』*
+║ *『 WOW YOU CHOOSEN 𝑮𝑪𝒀𝑩𝑬𝑹 𝑩𝑶𝑻 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚══════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
 ║❒ *Ytube:* _youtube.com/@JASTINMTEWA-vn9pl_
-║❒ *Owner:* _https://wa.me/message/A4QG2JZKBXFTN1_
-║❒ *Repo:* _https://github.com/Kingjux/venocyber-md_
-║❒ *WaGroup:* _https://chat.whatsapp.com/HSln3blDuuuKvC8njxyCCN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l_
-║❒ *Plugins:* _https://github.com/Kingjux/venocyber-md-plugins_
+║❒ *Owner:* _https://wa.link/kho6x6_
+║❒ *Repo:* _https://github.com/Gmaxhacker1/GCYBER-Md-v1_
+║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaFytPbAojYm7RIs6l1x_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaFytPbAojYm7RIs6l1x_
+║❒ *Plugins:* _https://github.com/Gmaxhacker1/GCYBER-Md-v1_
 ╚══════════════════════╝ 
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_Venocyber_Tech.sendMessage(Qr_Code_By_Venocyber_Tech.user.id,{text:Venocyber_MD_TEXT},{quoted:session})
+	 await Qr_Code_By_Gmax_Tech.sendMessage(Qr_Code_By_Gmax_Tech.user.id,{text:Gcyber_BOT_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Venocyber_Tech.ws.close();
+					await Qr_Code_By_Gmax_Tech.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
@@ -103,6 +103,6 @@ _Don't Forget To Give Star To My Repo_`
 			await removeFile("temp/" + id);
 		}
 	}
-	return await VENOCYBER_MD_QR_CODE()
+	return await GCYBER_BOT_QR_CODE()
 });
 module.exports = router
